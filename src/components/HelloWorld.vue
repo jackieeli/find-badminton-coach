e590141c325290bb9acd38dc7cd67c9395d25924
Jackie_Lee <63651674+marmeladeJackie@users.noreply.github.com>
2022-07-27 14:51:42 +0800

    tailwindcss has been added.

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,15 +1,15 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+import { ref } from "vue";
 
-defineProps<{ msg: string }>()
+defineProps<{ msg: string }>();
 
-const count = ref(0)
+const count = ref(0);
 </script>
 
 <template>
   <h1>{{ msg }}</h1>
 
-  <div class="card">
+  <div class="card bg-indigo-400">
     <button type="button" @click="count++">count is {{ count }}</button>
     <p>
       Edit
