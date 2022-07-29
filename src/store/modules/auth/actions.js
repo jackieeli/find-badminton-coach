795aa1d551f4b347604500d706278fcdb23c01ac
Jackie_Lee <63651674+marmeladeJackie@users.noreply.github.com>
2022-07-29 23:58:12 +0800

    init the login(authentication) module

--- src/store/modules/auth/actions.js
+++ src/store/modules/auth/actions.js
@@ -3,13 +3,50 @@ import axios from 'axios';
 let timer;
 
 export default {
-  auth(context, payload) {
-    const apikey = context.getters.apiKey;
+  async auth(context, payload) {
+    const apiKey = context.getters.apiKey;
     const mode = payload.mode;
 
-    let url = `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${apiKey}`;
+    let url = `https://identitytoolkit.googleapis.com/
+      v1/accounts:signInWithPassword?key=${apiKey}`;
     if (mode === 'signup') {
-      url = `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${apiKey}`;
+      url = `https://identitytoolkit.googleapis.com/
+        v1/accounts:signUp?key=${apiKey}`;
     }
-  }
-}
+
+    try {
+      const { data: responseData } = await axios.post(url, {
+        email: payload.email,
+        password: payload.password,
+        returnSecureToken: true,
+      });
+
+      const expiresIn = +responseData.expiresIn * 1000;
+      const expirationDate = Date.now() + expiresIn;
+
+      localStorage.setItem('token', responseData.idToken);
+      localStorage.setItem('userID', responseData.localId);
+      localStorage.setItem('tokenExpiration', expirationDate);
+
+      timer = setTimeout(() => {
+        context.dispatch('autoLogout');
+      }, expiresIn);
+
+      context.commit('setUser', {
+        token: responseData.idToken,
+        userID: responseData.localId,
+      });
+    } catch (err) {
+      const error = new Error(
+        err.response.data.error.message || 'Failed to authenticate'
+      );
+      throw error;
+    }
+  },
+
+  tryLogin(context) {
+    const token = localStorage.getItem('token');
+    const userID = localStorage.getItem('userID');
+    const tokenExpiration = localStorage.getItem('tokenExpiration');
+  },
+};
