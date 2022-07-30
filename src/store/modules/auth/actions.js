42d31fed75f1edcfecca27bdeef7b36e7c9bf189
Jackie_Lee <63651674+marmeladeJackie@users.noreply.github.com>
2022-07-30 23:42:14 +0800

    changed and improved the sign-in and sign-up steps

--- src/store/modules/auth/actions.js
+++ src/store/modules/auth/actions.js
@@ -3,6 +3,19 @@ import axios from 'axios';
 let timer;
 
 export default {
+  async login(context, payload) {
+    return context.dispatch('auth', {
+      ...payload,
+      mode: 'login',
+    });
+  },
+  async signup(context, payload) {
+    // return so our component still know if the promise is resolved.
+    return context.dispatch('auth', {
+      ...payload,
+      mode: 'signup',
+    });
+  },
   async auth(context, payload) {
     const apiKey = context.getters.apiKey;
     const mode = payload.mode;
@@ -43,10 +56,33 @@ export default {
       throw error;
     }
   },
-
+  // check if user is already in local storage.
   tryLogin(context) {
     const token = localStorage.getItem('token');
     const userID = localStorage.getItem('userID');
     const tokenExpiration = localStorage.getItem('tokenExpiration');
+
+    // how much time is left till expiring
+    const expiresIn = +tokenExpiration - Date.now();
+
+    // if timer expired in 10s, don't login in
+    if (expiresIn < 10000) return;
+
+    setTimeout(() => {
+      context.dispatch('autoLogout');
+    }, expiresIn);
+
+    if (token && userID) {
+      // login user automatically
+      context.commit('setUser', {
+        token,
+        userID,
+      });
+    }
+  },
+  logout(context) {
+    localStorage.removeItem('token');
+    localStorage.removeItem('userID');
+    localStorage.removeItem('tokenExpiration');
   },
 };
