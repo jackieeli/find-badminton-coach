a17feb23a7f906b2a758c92a26a20cdefe86500a
Jackie_Lee <63651674+marmeladeJackie@users.noreply.github.com>
2022-08-01 22:50:56 +0800

    has finished the vuex store!

--- src/store/index.js
+++ src/store/index.js
@@ -1,1 +1,25 @@
 import { createStore } from 'vuex';
+import coaches from './modules/coaches';
+import requests from './modules/requests';
+import auth from './modules/auth';
+
+const store = createStore({
+  modules: {
+    coaches,
+    requests,
+    auth,
+  },
+  state() {
+    return {
+      databaseUrl: 'xxx-xxx.firebaseio.com', /// waiting for the real url...
+    };
+  },
+  mutations: {},
+  getters: {
+    databaseUrl(state) {
+      return state.databaseUrl;
+    },
+  },
+});
+
+export default store;
