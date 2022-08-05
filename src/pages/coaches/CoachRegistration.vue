a559e97df75475c97288c82bf1a44a10ca3fd007
Jackie_Lee <63651674+marmeladeJackie@users.noreply.github.com>
2022-08-05 11:26:52 +0800

    has finished the CoachRegistration page!

---
+++ src/pages/coaches/CoachRegistration.vue
@@ -0,0 +1,35 @@
+<template>
+  <section class="row">
+    <base-card mode="shadow" color="color">
+      <h2>Register as a coach now!</h2>
+      <CoachForm @save-data="saveData" />
+    </base-card>
+  </section>
+</template>
+
+<script>
+// todo -> add error handling and loading spinner
+
+import CoachForm from '../../components/coaches/CoachForm.vue';
+
+export default {
+  components: { CoachForm },
+  methods: {
+    saveData(data) {
+      this.$store.dispatch('coaches/registerCoach', data);
+      this.$router.replace('/coaches');
+    },
+  },
+};
+</script>
+
+<style scoped>
+h2 {
+  margin-bottom: 1.6rem;
+  color: var(--purple-2);
+}
+
+section {
+  margin-top: 3.2rem;
+}
+</style>
