71b6c52b13a4202159df7377d43695d487145476
Jackie_Lee <63651674+marmeladeJackie@users.noreply.github.com>
2022-08-05 21:12:03 +0800

    add the RequestItem and RequestsReceived page.

---
+++ src/pages/requests/RequestsReceived.vue
@@ -0,0 +1,128 @@
+<template>
+  <div>
+    <base-dialog :show="!!error" title="An error occured" @close="handleError">
+      <p>{{ error }}</p>
+    </base-dialog>
+    <section class="row">
+      <div class="controls">
+        <base-button mode="flat" @click="loadRequests">Refresh</base-button>
+      </div>
+      <base-card color="color">
+        <header>
+          <h2>Requests Received</h2>
+          <h3 v-if="currentCoach" class="current">
+            Logged in as {{ currentCoach }}
+          </h3>
+        </header>
+        <base-spinner v-if="isLoading"></base-spinner>
+        <ul v-else-if="hasRequests && !isLoading">
+          <RequestItem
+            v-for="req in receivedRequests"
+            :key="req.id"
+            :email="req.userEmail"
+            :message="req.message"
+            :timestamp="req.timestamp"
+          />
+        </ul>
+        <h3 v-else>You haven't received any requests yet.</h3>
+      </base-card>
+    </section>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex';
+import RequestItem from './RequestItem.vue';
+
+export default {
+  components: {
+    RequestItem,
+  },
+  data() {
+    return {
+      isLoading: false,
+      error: null,
+      currentCoach: null,
+    };
+  },
+  computed: {
+    ...mapGetters('requests', {
+      receivedRequests: 'requests',
+      hasRequests: 'hasRequests',
+    }),
+  },
+  methods: {
+    async loadRequests() {
+      this.isLoading = true;
+
+      try {
+        await this.$store.dispatch('requests/fetchRequests');
+      } catch (err) {
+        this.error = err.message || 'Something went wrong';
+      }
+
+      this.isLoading = false;
+    },
+    handleError() {
+      this.error = null;
+    },
+    async getCurrentCoach() {
+      const userID = this.$store.getters.userID;
+
+      await this.$store.dispatch('coaches/loadCoaches', {
+        forceRefresh: false,
+      });
+
+      const coaches = this.$store.getters['coaches/coaches'];
+      const coach = coaches.find(coach => coach.id === userID);
+      this.currentCoach = coach.firstName;
+    },
+  },
+  created() {
+    this.loadRequests();
+    this.getCurrentCoach();
+  },
+};
+</script>
+
+<style scoped>
+section:first-of-type {
+  margin-top: 3.2rem;
+}
+
+header {
+  text-align: center;
+}
+
+ul {
+  max-width: 40rem;
+  margin: 0 auto;
+
+  display: flex;
+  flex-direction: column;
+  gap: 1.6rem;
+}
+
+h2 {
+  font-size: 2.4rem;
+  font-family: var(--font-display);
+  font-weight: normal;
+  margin-bottom: 1em;
+}
+
+h3 {
+  text-align: center;
+  font-size: 1.8rem;
+  margin-bottom: 0.8rem;
+}
+
+.current {
+  color: var(--purple-2);
+  margin-bottom: 2.4rem;
+}
+
+.controls {
+  display: flex;
+  justify-content: center;
+}
+</style>
